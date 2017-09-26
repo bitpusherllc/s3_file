a63f6c2bec676c1f7601d714c26154651c0477b7
Mike Doty <mdoty@bitpusher.com>
2017-09-26 15:55:03 -0700

    clean up for older chef

--- metadata.rb
+++ metadata.rb
@@ -9,8 +9,6 @@ version          "2.8.5"
 source_url 'https://github.com/adamsb6/s3_file' if responds_to(:source_url)
 issues_url 'https://github.com/adamsb6/s3_file/issues' if responds_to(:issues_url)
 
-chef_version '>= 12.6' if responds_to(:chef_version)
-
 supports 'centos'
 supports 'redhat'
 supports 'windows'
